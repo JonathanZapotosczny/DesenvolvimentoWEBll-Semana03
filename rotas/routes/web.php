98691570843aa43e10d001c04b2b605fbc065065
Jonathan Zapotosczny <jonathangm.hd@gmail.com>
2022-06-17 21:30:28 -0300

    Atualização...

--- rotas/routes/web.php
+++ rotas/routes/web.php
@@ -29,7 +29,7 @@ Route::prefix('/aluno')->group(function() {
             5 => "Pedro"
         );
 
-        $alunos = "<ul>";
+        $alunos = "<ol>";
 
         $cont = 0;
         foreach($dados as $nome) {
@@ -37,8 +37,8 @@ Route::prefix('/aluno')->group(function() {
             $cont++;
             if($cont >= count($dados)) break;
         }
-        
-        $alunos .= "</ul>";
+
+        $alunos .= "</ol>";
         return $alunos;
 
     })->name('aluno');
@@ -53,7 +53,7 @@ Route::prefix('/aluno')->group(function() {
             5 => "Pedro"
         );
 
-        $alunos = "<ul>";
+        $alunos = "<ol>";
 
         if($total <= count($dados)) {
             $cont = 0;
@@ -65,12 +65,12 @@ Route::prefix('/aluno')->group(function() {
         }else {
             $alunos = $alunos."<li>Tamanho Máximo = ".count($dados)."</li>";
         }
-        
-        
-        $alunos .= "</ul>";
-    
+
+
+        $alunos .= "</ol>";
+
         return $alunos;
-    })->name('aluno.limite');
+    })->name('aluno.limite')->where('total', '[0-9]+');
 
     Route::get('/matricula/{matricula}', function($matricula) {
 
@@ -82,20 +82,20 @@ Route::prefix('/aluno')->group(function() {
             5 => "Pedro"
         );
 
-        $alunos = "<ul>";
+        $alunos = "<ol>";
         if ($matricula <= count($dados)) {
             $alunos .= "<li>".$dados[$matricula]."</li>";
         }else {
             $alunos = $alunos."<li>NÃO ENCONTRADO!</li>";
         }
-        
 
-        $alunos .= "</ul>";
+
+        $alunos .= "</ol>";
 
         return $alunos;
-    
-        
-    })->name('aluno.matricula');
+
+
+    })->name('aluno.matricula')->where('matricula', '[0-9]+');
 
     Route::get('/nome/{nome}', function($nome) {
 
@@ -107,56 +107,60 @@ Route::prefix('/aluno')->group(function() {
             5 => "Pedro"
         );
 
-        $alunos = "<ul>";
+        $alunos = "<ol>";
 
         foreach($dados as $i) {
             if(strcmp($i, $nome) == 0) {
                 $alunos .= "<li>".$nome."</li>";
             }else {
                 $alunos .= $alunos."<li>NÃO ENCONTRADO!</li>";
+                break;
             }
         }
-        
-        $alunos .= "</ul>";
-    
+
+        $alunos .= "</ol>";
+
         return $alunos;
-    })->name('aluno.nome');
+    })->name('aluno.nome')->where('nome', '[A-Za-z]+');
 
-    Route::get('/nota', function() {
+});
 
-        $header = "<table><tr><td><Strong>Matrícula&emsp;</Strong></td><td><Strong>Aluno&emsp;</Strong></td><td><Strong>Nota</Strong></td></tr>";
+Route::prefix('/nota')->group(function() {
+    Route::get('/', function() {
 
-        $dados = array(
-            array('matricula'=> 1, 'nome'=> "Jonathan", "nota"=> 8),
-            array('matricula'=> 2, 'nome'=> "Karoline", "nota"=> 10),
-            array('matricula'=> 3, 'nome'=> "Edevaldo", "nota"=> 9),
-            array('matricula'=> 4, 'nome'=> "Anderson", "nota"=> 7),
-            array('matricula'=> 5, 'nome'=> "Pedro", "nota"=> 6),
-        );
+    $header = "<table><tr><td><Strong>Matrícula&emsp;</Strong></td><td><Strong>Aluno&emsp;</Strong></td><td><Strong>Nota</Strong></td></tr>";
 
-        foreach($dados as $aluno) {
-            $header .= "<tr>";
-            foreach ($aluno as $key => $value) {
-                $header .= "<td>$value</td>";
-            }
-            $header .= "</tr>";
+    $dados = array(
+        array('matricula'=> 1, 'nome'=> "Jonathan", "nota"=> 9),
+        array('matricula'=> 2, 'nome'=> "Karoline", "nota"=> 2),
+        array('matricula'=> 3, 'nome'=> "Edevaldo", "nota"=> 10),
+        array('matricula'=> 4, 'nome'=> "Anderson", "nota"=> 6),
+        array('matricula'=> 5, 'nome'=> "Pedro", "nota"=> 4),
+    );
+
+    foreach($dados as $aluno) {
+        $header .= "<tr>";
+        foreach ($aluno as $key => $value) {
+            $header .= "<td>$value</td>";
         }
+        $header .= "</tr>";
+    }
 
-        $header .= "</ul></table>";
-        return $header;
+    $header .= "</ul></table>";
+    return $header;
 
-    })->name('aluno.nota');
+    })->name('nota');
 
-    Route::get('/nota/limite/{limite}', function($limite) {
+    Route::get('limite/{limite}', function($limite) {
 
         $header = "<table><tr><td><Strong>Matrícula&emsp;</Strong></td><td><Strong>Aluno&emsp;</Strong></td><td><Strong>Nota</Strong></td></tr>";
 
         $dados = array(
-            array('matricula'=> 1, 'nome'=> "Jonathan", "nota"=> 8),
-            array('matricula'=> 2, 'nome'=> "Karoline", "nota"=> 10),
-            array('matricula'=> 3, 'nome'=> "Edevaldo", "nota"=> 9),
-            array('matricula'=> 4, 'nome'=> "Anderson", "nota"=> 7),
-            array('matricula'=> 5, 'nome'=> "Pedro", "nota"=> 6),
+            array('matricula'=> 1, 'nome'=> "Jonathan", "nota"=> 9),
+            array('matricula'=> 2, 'nome'=> "Karoline", "nota"=> 2),
+            array('matricula'=> 3, 'nome'=> "Edevaldo", "nota"=> 10),
+            array('matricula'=> 4, 'nome'=> "Anderson", "nota"=> 6),
+            array('matricula'=> 5, 'nome'=> "Pedro", "nota"=> 4),
         );
 
         if ($limite <= count($dados)) {
@@ -170,11 +174,148 @@ Route::prefix('/aluno')->group(function() {
                     if($cont >= $limite) break;
                     $header .= "</tr>";
                 }
+        } else {
+            $header = $header."<li>Tamanho Máximo = ".count($dados)."</li>";
         }
 
         $header .= "</ul></table>";
         return $header;
 
-    })->name('aluno.nota.limite');
+    })->name('nota.limite')->where('limite', '[0-9]+');
+
+    Route::get('lancar/{nota}/{matricula}/{nome?}', function($nota, $matricula, $nome=null) {
+
+        $header = "<table><tr><td><Strong>Matrícula&emsp;</Strong></td><td><Strong>Aluno&emsp;</Strong></td><td><Strong>Nota</Strong></td></tr>";
+
+        $dados = array(
+            array('matricula'=> 1, 'nome'=> "Jonathan", "nota"=> 9),
+            array('matricula'=> 2, 'nome'=> "Karoline", "nota"=> 2),
+            array('matricula'=> 3, 'nome'=> "Edevaldo", "nota"=> 10),
+            array('matricula'=> 4, 'nome'=> "Anderson", "nota"=> 6),
+            array('matricula'=> 5, 'nome'=> "Pedro", "nota"=> 4),
+        );
+
+        $aux = $dados;
+
+        if($nome == null) {
+
+        $indice = array_search($matricula, array_column($aux, 'matricula'));
+
+        $alterado = [
+            'matricula' => $matricula,
+            'nome' => $dados[$indice]['nome'],
+            'nota' => $nota
+        ];
+
+        $dados[$indice] = $alterado;
+
+        }else {
+
+            $indice = array_search($nome, array_column($aux, 'nome'));
+            $indiceM = array_search($matricula, array_column($aux, 'matricula'));
+
+            if($indiceM != null) {
+            
+            $alterado = [
+                'matricula' => $matricula,
+                'nome' => $nome,
+                'nota' => $nota
+            ];
+
+            $dados[$indice] = $alterado;
+
+        } else {
+           
+            echo "<h2> Matrícula NÃO EXISTE! </h2>";
+        }
+
+        }
+
+        foreach($dados as $aluno) {
+            $header .= "<tr>";
+            foreach ($aluno as $key => $value) {
+                $header .= "<td>$value</td>";
+            }
+            $header .= "</tr>";
+        }
+
+        $header .= "</ul></table>";
+        return $header;
+
+    })->name('nota.lancar');
+
+    Route::get('conceito/{A}/{B}/{C}', function($a, $b, $c) {
+
+        $header = "<table><tr><td><Strong>Matrícula&emsp;</Strong></td><td><Strong>Aluno&emsp;</Strong></td><td><Strong>Nota</Strong></td></tr>";
+    
+        $dados = array(
+            array('matricula'=> 1, 'nome'=> "Ana", "nota"=> 9),
+            array('matricula'=> 2, 'nome'=> "Bruno", "nota"=> 2),
+            array('matricula'=> 3, 'nome'=> "Carol", "nota"=> 10),
+            array('matricula'=> 4, 'nome'=> "Danilo", "nota"=> 6),
+            array('matricula'=> 5, 'nome'=> "Ellen", "nota"=> 4),
+        );
+    
+     
+        foreach($dados as $aluno) {
+            if($aluno["nota"] >= $a){
+                $aluno["nota"] = "A";
+            }
+            else if($aluno["nota"] >= $b && $aluno["nota"] < $a){
+                $aluno["nota"] = "B";
+            }
+            else if($aluno["nota"] >= $c && $aluno["nota"] < $b && $aluno["nota"] < $a){
+                $aluno["nota"] = "C";
+            }
+            else {
+                $aluno["nota"] = "D";
+            }
+            foreach ($aluno as $key => $value) {
+                $header .= "<td>$value</td>";
+            }
+            $header .= "</tr>";
+        }
+    
+        $header .= "</ul></table>";
+        return $header;
+    
+    })->name('nota.conceito');
+
+    Route::post('conceito/{A}/{B}/{C}', function($a, $b, $c) {
+        @csrf;
+        $header = "<table><tr><td><Strong>Matrícula&emsp;</Strong></td><td><Strong>Aluno&emsp;</Strong></td><td><Strong>Nota</Strong></td></tr>";
+    
+        $dados = array(
+            array('matricula'=> 1, 'nome'=> "Ana", "nota"=> 9),
+            array('matricula'=> 2, 'nome'=> "Bruno", "nota"=> 2),
+            array('matricula'=> 3, 'nome'=> "Carol", "nota"=> 10),
+            array('matricula'=> 4, 'nome'=> "Danilo", "nota"=> 6),
+            array('matricula'=> 5, 'nome'=> "Ellen", "nota"=> 4),
+        );
+    
+     
+        foreach($dados as $aluno) {
+            if($aluno["nota"] >= $a){
+                $aluno["nota"] = "A";
+            }
+            else if($aluno["nota"] >= $b && $aluno["nota"] < $a){
+                $aluno["nota"] = "B";
+            }
+            else if($aluno["nota"] >= $c && $aluno["nota"] < $b && $aluno["nota"] < $a){
+                $aluno["nota"] = "C";
+            }
+            else {
+                $aluno["nota"] = "D";
+            }
+            foreach ($aluno as $key => $value) {
+                $header .= "<td>$value</td>";
+            }
+            $header .= "</tr>";
+        }
+    
+        $header .= "</ul></table>";
+        return $header;
+    
+    })->name('nota.conceito');
 
 });
